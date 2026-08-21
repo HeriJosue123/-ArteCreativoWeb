231955decde662f527325aa6a45bedb065a99ff0
Heriberto Josue Echeverria Cardona <ec1149032024@unab.edu.sv>
2026-08-20 22:00:23 -0600

    feat: enable floating WhatsApp contact button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Categorias } from "./components/Categorias";
 import { ImpactoVisual } from "./components/ImpactoVisual";
 import { CallToAction } from "./components/CallToAction";
 import { Intro } from "./components/Intro";
+import WhatsAppContact from "./components/WhatsAppContact";
 
 function App() {
   return (
@@ -16,9 +17,9 @@ function App() {
       <Categorias />
       <ImpactoVisual />
       <CallToAction />
+      <WhatsAppContact />
     </div>
   );
 }
 
 export default App;
-
